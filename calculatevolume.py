cee55ead41eb47dc775167c022dcc01a345b0982
orbina <48756153+orbina@users.noreply.github.com>
2020-12-15 21:54:45 +0100

    Create calculatevolume.py

---
+++ calculatevolume.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print('Calculate volume of a rectangular tank.')
+le = input('Enter length of tank (cm): ')
+wi = input('Enter width of tank (cm): ')
+he = input('Enter height of tank (cm): ')
+print('Volume (in cm2) of the tank is:')
+print(le*wi*he)
