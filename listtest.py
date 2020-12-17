09b6a814279a55f49740896155e632224775f256
orbina <48756153+orbina@users.noreply.github.com>
2020-12-17 13:54:40 +0100

    Update listtest.py

--- listtest.py
+++ listtest.py
@@ -5,13 +5,14 @@ print('Welcome ' + os.getlogin())
 
 friends = []
 i = 0
-maxLengthList = 10
+maxLengthList = 3
 while len(friends) < maxLengthList:
         i += 1
         name = input('Enter name %d: ' %i)
         if name == '':
             exit('Error, name not added. Exiting.')
         friends.append(name)
+friends.sort()
 
 search_list = input('Would you like to search for a name? Y/n/show all: ')
 if search_list.lower() == 'y' or search_list.lower() == 'yes':
