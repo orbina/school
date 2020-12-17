8bce4df61fce8d0791f5f56258cc4b7bac538986
orbina <48756153+orbina@users.noreply.github.com>
2020-12-17 13:29:32 +0100

    Create listtest.py

---
+++ listtest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+
+print('Welcome ' + os.getlogin())
+
+friends = []
+i = 0
+maxLengthList = 10
+while len(friends) < maxLengthList:
+        i += 1
+        name = input('Enter name %d: ' %i)
+        if name == '':
+            exit('Error, name not added. Exiting.')
+        friends.append(name)
+
+search_list = input('Would you like to search for a name? Y/n/show all: ')
+if search_list.lower() == 'y' or search_list.lower() == 'yes':
+    search = input('Enter name to search for: ')
+    if search in friends:
+        print('Friend is found')
+elif search_list.lower() == 'n' or search_list.lower() == 'no':
+    exit('Not searching, exiting.')
+elif search_list.lower() == 'a' or search_list.lower() == 'all' or search_list.lower() == 's' or search_list.lower() == 'show' or search_list.lower() == 'show all':
+    print(*friends)
+else:
+    print('Friend is not found')
