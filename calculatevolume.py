93662a24fccc5b670ff0fa44cb1df32869b24029
orbina <48756153+orbina@users.noreply.github.com>
2020-12-17 10:30:51 +0100

    Update calculatevolume.py

--- calculatevolume.py
+++ calculatevolume.py
@@ -4,7 +4,7 @@ print('Calculate volume of a rectangular tank.')
 le = input('Enter length of tank (cm): ')
 wi = input('Enter width of tank (cm): ')
 he = input('Enter height of tank (cm): ')
-volume = le*int(wi)*int(he)
+volume = int(le)*int(wi)*int(he)
 if volume != 0:
     print('Volume (in cm2) of the tank is: ' + str(volume) + ' cm2.')
 elif volume == 0:
