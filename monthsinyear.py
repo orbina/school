1c6dfcae0b3da3a76099e36d8fa655fdef66d4fe
orbina <48756153+orbina@users.noreply.github.com>
2021-01-05 22:45:51 +0100

    Update monthsinyear.py

--- monthsinyear.py
+++ monthsinyear.py
@@ -12,13 +12,13 @@ def month():
     elif choice == '3': # Print only first letter of each month
         for letter in months:
             print(letter[0])
-    else: # Invalid option, exiting program
+    else:
         print('Sorry, non capisco un cazzo.')
 
 
 print('Choose')
 print('1. Months in the year')
 print('2. Months sorted alphabetically')
-print('3. Months by first letter')
+print('3. First letter of the months')
 print('Enter your choice:')
 month()
