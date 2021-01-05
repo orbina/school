54e39b1a3a6f19e7b75a629d287b77dbe6218c05
orbina <48756153+orbina@users.noreply.github.com>
2021-01-05 22:02:25 +0100

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -5,13 +5,13 @@ s_list = list()
 
 def shopping_list():
     name = input('Enter item: ')
-    s_list.append(name)
+    s_list.append(name) # Add item to list
     amount = int(input('Enter amount: '))
-    s_list.append(amount)
+    s_list.append(amount) # Add amount to list
 
 
 print('Welcome to the shopping list?')
-print('Press Q to exit.')
-while input('Add items? ') != 'Q':
+print('Press "n" to exit.')
+while input('Add items? Y/n ') != 'n': # Loop the list
     shopping_list()
-print(s_list, sep=' ')
+print(s_list, sep=' ') # Print finished list
