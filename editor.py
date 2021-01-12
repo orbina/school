4dc2cf038bcf993e19ad658553d3cd9f857822c9
orbina <48756153+orbina@users.noreply.github.com>
2021-01-12 22:09:54 +0100

    Update editor.py

--- editor.py
+++ editor.py
@@ -1,23 +1,20 @@
 #!/bin/usr/env python3
 
-the_string = input('Please enter your string: ')  # Adding a string used
-print('For menu, press \'m\'')
-
-
 def __editor():
-    __input = 0  # __input given the value '0' for the loop be valid
+    the_string = input('Please enter your string: ')  # Adding a string to be used
+    print('For menu, press \'m\'')
+    __input = 0  # __input given the value '0' for the loop to be valid
     while __input != 'e':  # Start of the loop
         print()
-        print('Current text: ', the_string)
+        print('Current text:', the_string)
         print()
         __input = input('Enter choice: ')  # Input for choices given
         if __input.lower() == 'e':  # Exit program
             exit('Exiting program.')
         elif __input.lower() == 'm':  # Print menu, imported from the __menu function
             print(__menu())
-        # # Couldn't manage to make the part under working
-        # elif __input.lower() == 'n':  # Add new string
-        #     the_string = input('Please enter new string: ')
+        elif __input.lower() == 'n':  # Update the string to be used
+            the_string = input('Please enter new string: ')
         elif __input == '1':  # Input given to display the string as uppercase
             print(the_string.upper())
         elif __input == '2':  # Input given to display the string as lowercase
@@ -30,13 +27,13 @@ def __editor():
             print('Invalid choice, please try again.')
 
 
-def __menu():  # Displaying the menu, with choices given
+def __menu():  # Displaying the menu, with choices available
     print('1: Uppercase')
     print('2: Lowercase')
     print('3: Title case')
     print('4: Remove front and back whitespaces')
     print('e: Exit program')
-    # print('n: Update string')
+    print('n: Update the string')
 
 
 print(__editor())
